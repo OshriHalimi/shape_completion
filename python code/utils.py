4d6a1228c4382e49ac55c5ec3d7e476b9796bc97
orlitany <or.litany@gmail.com>
2019-10-08 22:04:00 -0700

    added save ply to utils

--- python code/utils.py
+++ python code/utils.py
@@ -3,6 +3,21 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
+try:
+    from plyfile import PlyData, PlyElement
+except:
+    print("Please install the module 'plyfile' for PLY i/o, e.g.")
+    print("pip install plyfile")
+    sys.exit(-1)
+
+
+def write_ply(points, filename, text=True):
+    """ input: Nx3, write points to filename as PLY format. """
+    points = [(points[i,0], points[i,1], points[i,2]) for i in range(points.shape[0])]
+    vertex = np.array(points, dtype=[('x', 'f4'), ('y', 'f4'),('z', 'f4')])
+    el = PlyElement.describe(vertex, 'vertex', comments=['vertices'])
+    PlyData([el], text=text).write(filename)
+
 def calc_euclidean_dist_matrix(x):
     #OH: x contains the coordinates of the mesh,
     #x dimensions are [batch_size x num_nodes x 3]
