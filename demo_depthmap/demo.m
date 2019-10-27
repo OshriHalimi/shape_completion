7d666cee94d3351d2ac9ca9cee3ef38a29b2c0de
Giovanni <giovanni.trappolini@gmail.com>
2019-10-27 21:11:59 +0100

    matlab rangemaps parallel added

--- demo_depthmap/demo.m
+++ demo_depthmap/demo.m
@@ -3,6 +3,8 @@ clc
 close all
 clear all
 
+
+
 addpath('./tools/')
 
 shapes_dir = './data/';
@@ -14,10 +16,15 @@ for i=1:length(shapes)
     [~,nm,~] = fileparts(shapes(i).name);
     
     full_model = load_off(sprintf('%s/%s', shapes_dir, shapes(i).name));
+    full_model.VERT = full_model.VERT * 80;
     full_model.S_tri = calc_tri_areas(full_model);
+    full_model.VERT
     full_model.VERT = full_model.VERT - repmat(mean(full_model.VERT),full_model.n,1);
     
-    for k=1:4 % four elevations
+    
+    
+    
+    for k=1:1 % four elevations
         
         null = full_model;
         
@@ -26,26 +33,31 @@ for i=1:length(shapes)
         end
         
         % five random rotations
-        angles = linspace(0,2*pi,13);
-        angles = angles(1:end-1);
-        angles = angles(randperm(length(angles)));
-        angles = angles(1:5);
+        n_cores = 4;
+        angle_offset = rand() * 2*pi;
+        angles = (1:n_cores) * 2*pi / n_cores + angle_offset;
+        % angles = angles(randperm(length(angles)));
+        % angles = angles(1:5)
         
-        for ai=1:length(angles)
+        
+        tic
+        parfor ai=1:length(angles)
             
-            fprintf('Shape %d/%d, view %d/%d...\n', i, length(shapes), (k-1)*5+ai, 4*length(angles));
+            fprintf('Shape %d/%d, view %d/%d...\n', i, length(shapes), (k-1)*5+ai, length(angles));
             
             a = angles(ai);
 
             S = null;
-            S.VERT = S.VERT * [1 0 0 ; 0 0 -1 ; 0 1 0];
+            %S.VERT = S.VERT * [1 0 0 ; 0 0 1 ; 0 1 0];
 %             coef = 20 / range(S.VERT(:,1));
-            coef = 0.3*sqrt(sum(S.S_tri)) / max(range(S.VERT));
+            coef = 0.15*sqrt(sum(S.S_tri)) / max(range(S.VERT));
             S.VERT = coef.*S.VERT;
             
+            %S.VERT = S.VERT * [cos(a) -sin(a) 0 ; sin(a) cos(a) 0 ; 0 0 1];
             S.VERT = S.VERT * [cos(a) 0 -sin(a) ; 0 1 0 ; sin(a) 0 cos(a)];
             
             [M, depth, matches] = create_rangemap(S, 95, 95, 3+randi(30));
+            
             M.gt = matches;
             
             M.VERT = M.VERT - repmat(mean(M.VERT),M.n,1);
@@ -57,9 +69,9 @@ for i=1:length(shapes)
             end
             
             out_fname = sprintf('%s/%s.range%d.mat', out_dir, nm, (k-1)*5+ai);
-            save(out_fname, 'M', 'depth');
+            parsave(out_fname, M, depth);
             
-            fig = figure;
+            fig = figure('visible', 'off');
             subplot(121), imagesc(depth), axis equal, colormap(gray), colorbar, title(sprintf('angle %.4f',a)), axis image
             subplot(122), plot_mesh(M), axis off; view([90 -90]); shading faceted
             fig_fname = sprintf('%s/%s.range%d.png', out_dir, nm, (k-1)*5+ai);
@@ -67,5 +79,6 @@ for i=1:length(shapes)
             autocrop_image(fig_fname, true);
             
         end % next rotation
+        toc
     end % next elevation
 end % next shape
---
+++ demo_depthmap/tools/parsave.m
@@ -0,0 +1,11 @@
+function parsave(fname, inp1, inp2)
+    
+    save(fname, 'inp1', 'inp2');
+    %var_name=genvarname(inputname(2)); 
+    %eval([var_name '=data;']);
+
+    %try 
+    %save(fname,var_name,'-append'); 
+    %catch 
+    %save(fname,var_name); 
+end
