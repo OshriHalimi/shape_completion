a2840735e4e71f8ce926108759dcdd399e5fdecc
Amit <amitbracha0@gmail.com>
2019-11-05 18:52:50 +0200

    Added offline save. Now the epoch last batch is optionally saved to a folder

--- python code/train_sup.py
+++ python code/train_sup.py
@@ -8,6 +8,8 @@ import os
 import json
 import visdom
 import time
+import matplotlib.pyplot as plt
+import scipy.io as sio
 
 from dataset import *
 from model import *
@@ -20,12 +22,23 @@ if __name__ == '__main__':  # OH: Wrapping the main code with __main__ check is 
     parser.add_argument('--batchSize', type=int, default=15, help='input batch size')
     parser.add_argument('--workers', type=int, help='number of data loading workers', default=8)
     parser.add_argument('--nepoch', type=int, default=1000, help='number of epochs to train for')
-    parser.add_argument('--model', type=str, default=os.path.join(os.getcwd(), "log", "Simple network; Translation augmentation; Input normals; Deeper Decoder", "network_last.pth"), help='optional reload model path')
-    parser.add_argument('--save_path', type=str, default='Simple network; Translation augmentation; Input normals; Deeper Decoder', help='save path')
-    parser.add_argument('--env', type=str, default="3DCODED_supervised", help='visdom environment')  #OH: TODO edit
+    parser.add_argument('--model', type=str, default=os.path.join(os.getcwd(), "log",
+                                                                  "Simple network; Translation augmentation; Input normals; Deeper Decoder",
+                                                                  "network_last.pth"),
+                        help='optional reload model path')
+    parser.add_argument('--save_path', type=str,
+                        default='Simple network; Translation augmentation; Input normals; Deeper Decoder',
+                        help='save path')
+    parser.add_argument('--env', type=str, default="3DCODED_supervised", help='visdom environment')  # OH: TODO edit
+    parser.add_argument('--saveOffline', type=bool, default=False)
 
     opt = parser.parse_args()
     print(opt)
+    ts = 0
+    if opt.saveOffline:
+        ts = time.time()
+        ts = int(ts)
+        os.mkdir(ts)
 
     # =============DEFINE stuff for logs ======================================== #
     # Launch visdom for visualization
@@ -38,7 +51,7 @@ if __name__ == '__main__':  # OH: Wrapping the main code with __main__ check is 
 
     blue = lambda x: '\033[94m' + x + '\033[0m'
 
-    opt.manualSeed = 1 #random.randint(1, 10000)  # fix seed
+    opt.manualSeed = 1  # random.randint(1, 10000)  # fix seed
     print("Random Seed: ", opt.manualSeed)
     random.seed(opt.manualSeed)
     torch.manual_seed(opt.manualSeed)
@@ -54,10 +67,12 @@ if __name__ == '__main__':  # OH: Wrapping the main code with __main__ check is 
     # ===================CREATE DATASET================================= #
 
     dataset = AmassProjectionsDataset(train=True)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize, shuffle=True, num_workers=int(opt.workers), pin_memory=True)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize, shuffle=True,
+                                             num_workers=int(opt.workers), pin_memory=True)
     # OH: pin_memory=True used to increase the performance when transferring the fetched data from CPU to GPU
     dataset_test = AmassProjectionsDataset(train=False)
-    dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=opt.batchSize, shuffle=True, num_workers=int(opt.workers), pin_memory=True)
+    dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=opt.batchSize, shuffle=True,
+                                                  num_workers=int(opt.workers), pin_memory=True)
     len_dataset = len(dataset)
     len_dataset_test = len(dataset_test)
 
@@ -72,7 +87,7 @@ if __name__ == '__main__':  # OH: Wrapping the main code with __main__ check is 
     # ========================================================== #
 
     # ===================CREATE optimizer================================= #
-    lrate = 0.001 # learning rate
+    lrate = 0.001  # learning rate
     optimizer = optim.Adam(network.parameters(), lr=lrate)
 
     with open(logname, 'a') as f:  # open and append
@@ -81,8 +96,13 @@ if __name__ == '__main__':  # OH: Wrapping the main code with __main__ check is 
 
     # =============start of the learning loop ======================================== #
     for epoch in range(opt.nepoch):
+        part = None
+        template = None
+        pointsReconstructed = None
+        gt = None
+
         if (epoch % 100) == 99:
-            lrate = lrate/2.0
+            lrate = lrate / 2.0
             optimizer = optim.Adam(network.parameters(), lr=lrate)
 
         # TRAIN MODE
@@ -99,11 +119,11 @@ if __name__ == '__main__':  # OH: Wrapping the main code with __main__ check is 
 
             # Forward pass
             pointsReconstructed = network(part, template).double()
-            #D_reconstructed = calc_euclidean_dist_matrix(pointsReconstructed).double()
-            #D_gt = calc_euclidean_dist_matrix(gtV).double()
-            #loss_euclidean = torch.mean((D_reconstructed - D_gt) ** 2)
+            # D_reconstructed = calc_euclidean_dist_matrix(pointsReconstructed).double()
+            # D_gt = calc_euclidean_dist_matrix(gtV).double()
+            # loss_euclidean = torch.mean((D_reconstructed - D_gt) ** 2)
 
-            loss_points = torch.mean((pointsReconstructed[:,:3,:] - gt[:,:3,:].double()) ** 2)
+            loss_points = torch.mean((pointsReconstructed[:, :3, :] - gt[:, :3, :].double()) ** 2)
 
             loss_net = loss_points
             loss_net.backward()
@@ -114,16 +134,41 @@ if __name__ == '__main__':  # OH: Wrapping the main code with __main__ check is 
             if i % 100 == 0:
                 # VIZUALIZE
                 if i % 100 == 0:
-                    vis.scatter(X=part[0,:3,:].transpose(1, 0).contiguous().data.cpu(), win='Train_Part',
+                    vis.scatter(X=part[0, :3, :].transpose(1, 0).contiguous().data.cpu(), win='Train_Part',
                                 opts=dict(title="Train_Part", markersize=2, ), )
-                    vis.scatter(X=template[0,:3,:].transpose(1, 0).contiguous().data.cpu(), win='Train_Template',
+                    vis.scatter(X=template[0, :3, :].transpose(1, 0).contiguous().data.cpu(), win='Train_Template',
                                 opts=dict(title="Train_Template", markersize=2, ), )
                     vis.scatter(X=pointsReconstructed[0].transpose(1, 0).contiguous().data.cpu(), win='Train_output',
-                                opts=dict(title="Train_output",markersize=2, ), )
-                    vis.scatter(X=gt[0,:3,:].transpose(1, 0).contiguous().data.cpu(), win='Train_Ground_Truth',
+                                opts=dict(title="Train_output", markersize=2, ), )
+                    vis.scatter(X=gt[0, :3, :].transpose(1, 0).contiguous().data.cpu(), win='Train_Ground_Truth',
                                 opts=dict(title="Train_Ground_Truth", markersize=2, ), )
 
-            print('[%d: %d/%d] train loss:  %f' % (epoch, i, len_dataset / opt.batchSize,  loss_net.item()))
+            print('[%d: %d/%d] train loss:  %f' % (epoch, i, len_dataset / opt.batchSize, loss_net.item()))
+        if opt.saveOffline:
+            for i in range(opt.batchSize):
+                tmp_fig = plt.figure()
+                ax_train_part = tmp_fig.add_subplot(141, projection='3d')
+                ax_train_template = tmp_fig.add_subplot(142, projection='3d')
+                ax_train_output = tmp_fig.add_subplot(143, projection='3d')
+                ax_train_ground_truth = tmp_fig.add_subplot(144, projection='3d')
+                ax_train_part.scatter(part[0, 0, :].contiguous().data.cpu(),
+                                      part[0, 1, :].contiguous().data.cpu(),
+                                      part[0, 2, :].contiguous().data.cpu())
+                ax_train_template.scatter(template[0, 0, :].contiguous().data.cpu(),
+                                      template[0, 1, :].contiguous().data.cpu(),
+                                      template[0, 2, :].contiguous().data.cpu())
+                ax_train_output.scatter(pointsReconstructed[0, 0, :].contiguous().data.cpu(),
+                                          pointsReconstructed[0, 1, :].contiguous().data.cpu(),
+                                          pointsReconstructed[0, 2, :].contiguous().data.cpu())
+                ax_train_ground_truth.scatter(gt[0, 0, :].contiguous().data.cpu(),
+                                          gt[0, 1, :].contiguous().data.cpu(),
+                                          gt[0, 2, :].contiguous().data.cpu())
+                plt.savefig(os.path.join(str(ts),'train_'+str(epoch)+'_'+str(i)+'.png'))
+            sio.savemat('train_'+str(epoch)+'.mat',{"Train_Part":part[:, :3, :].contiguous().data.cpu(),
+                                       "Train_Template":template[:, :3, :].contiguous().data.cpu(),
+                                       "Train_output":pointsReconstructed[0, :3, :].contiguous().data.cpu(),
+                                       "Train_Ground_Truth":gt[0, :3, :].contiguous().data.cpu()})
+
 
         # Validation
         with torch.no_grad():
@@ -142,25 +187,50 @@ if __name__ == '__main__':  # OH: Wrapping the main code with __main__ check is 
                 # D_gt = calc_euclidean_dist_matrix(gtV).double()
                 # loss_euclidean = torch.mean((D_reconstructed - D_gt) ** 2)
 
-                loss_points = torch.mean((pointsReconstructed - gt[:,:3,:].double()) ** 2)
+                loss_points = torch.mean((pointsReconstructed - gt[:, :3, :].double()) ** 2)
 
                 loss_net = loss_points
                 val_loss_L2_smpl.update(loss_net.item())
 
                 # VIZUALIZE
                 if i % 100 == 0:
-                    vis.scatter(X=part[0,:3,:].transpose(1, 0).contiguous().data.cpu(), win='Test_Part',
+                    vis.scatter(X=part[0, :3, :].transpose(1, 0).contiguous().data.cpu(), win='Test_Part',
                                 opts=dict(title="Test_Part", markersize=2, ), )
-                    vis.scatter(X=template[0,:3,:].transpose(1, 0).contiguous().data.cpu(), win='Test_Template',
+                    vis.scatter(X=template[0, :3, :].transpose(1, 0).contiguous().data.cpu(), win='Test_Template',
                                 opts=dict(title="Test_Template", markersize=2, ), )
                     vis.scatter(X=pointsReconstructed[0].transpose(1, 0).contiguous().data.cpu(), win='Test_output',
-                                opts=dict(title="Test_output",markersize=2, ), )
-                    vis.scatter(X=gt[0,:3,:].transpose(1, 0).contiguous().data.cpu(), win='Test_Ground_Truth',
+                                opts=dict(title="Test_output", markersize=2, ), )
+                    vis.scatter(X=gt[0, :3, :].transpose(1, 0).contiguous().data.cpu(), win='Test_Ground_Truth',
                                 opts=dict(title="Test_Ground_Truth", markersize=2, ), )
 
                 print('[%d: %d/%d] test smlp loss:  %f' % (epoch, i, len_dataset_test / opt.batchSize, loss_net.item()))
-
-
+            if opt.saveOffline:
+                for i in range(opt.batchSize):
+                    tmp_fig = plt.figure()
+                    ax_test_part = tmp_fig.add_subplot(141, projection='3d')
+                    ax_test_template = tmp_fig.add_subplot(142, projection='3d')
+                    ax_test_output = tmp_fig.add_subplot(143, projection='3d')
+                    ax_test_ground_truth = tmp_fig.add_subplot(144, projection='3d')
+                    ax_test_part.scatter(part[0, 0, :].contiguous().data.cpu(),
+                                          part[0, 1, :].contiguous().data.cpu(),
+                                          part[0, 2, :].contiguous().data.cpu())
+                    ax_test_template.scatter(template[0, 0, :].contiguous().data.cpu(),
+                                              template[0, 1, :].contiguous().data.cpu(),
+                                              template[0, 2, :].contiguous().data.cpu())
+                    ax_test_output.scatter(pointsReconstructed[0, 0, :].contiguous().data.cpu(),
+                                            pointsReconstructed[0, 1, :].contiguous().data.cpu(),
+                                            pointsReconstructed[0, 2, :].contiguous().data.cpu())
+                    ax_test_ground_truth.scatter(gt[0, 0, :].contiguous().data.cpu(),
+                                                  gt[0, 1, :].contiguous().data.cpu(),
+                                                  gt[0, 2, :].contiguous().data.cpu())
+                    plt.savefig(os.path.join(str(ts), 'test_' + str(epoch) + '_' + str(i) + '.png'))
+                sio.savemat('test_' + str(epoch) + '.mat', {"Test_Part": part[:, :3, :].contiguous().data.cpu(),
+                                                             "Test_Template": template[:, :3,
+                                                                               :].contiguous().data.cpu(),
+                                                             "Test_output": pointsReconstructed[0, :3,
+                                                                             :].contiguous().data.cpu(),
+                                                             "Test_Ground_Truth": gt[0, :3,
+                                                                                   :].contiguous().data.cpu()})
             # UPDATE CURVES
             L2curve_train_smpl.append(train_loss_L2_smpl.avg)
             L2curve_val_smlp.append(val_loss_L2_smpl.avg)
@@ -168,12 +238,12 @@ if __name__ == '__main__':  # OH: Wrapping the main code with __main__ check is 
             vis.line(X=np.column_stack((np.arange(len(L2curve_train_smpl)), np.arange(len(L2curve_val_smlp)))),
                      Y=np.column_stack((np.array(L2curve_train_smpl), np.array(L2curve_val_smlp))),
                      win='loss',
-                     opts=dict(title="loss", legend=["L2curve_train_smpl" + opt.env,"L2curve_val_smlp" + opt.env,]))
+                     opts=dict(title="loss", legend=["L2curve_train_smpl" + opt.env, "L2curve_val_smlp" + opt.env, ]))
 
             vis.line(X=np.column_stack((np.arange(len(L2curve_train_smpl)), np.arange(len(L2curve_val_smlp)))),
                      Y=np.log(np.column_stack((np.array(L2curve_train_smpl), np.array(L2curve_val_smlp)))),
                      win='log',
-                     opts=dict(title="log", legend=["L2curve_train_smpl" + opt.env,"L2curve_val_smlp" + opt.env,]))
+                     opts=dict(title="log", legend=["L2curve_train_smpl" + opt.env, "L2curve_val_smlp" + opt.env, ]))
 
             # dump stats in log file
             log_table = {
@@ -186,5 +256,5 @@ if __name__ == '__main__':  # OH: Wrapping the main code with __main__ check is 
             print(log_table)
             with open(logname, 'a') as f:  # open and append
                 f.write('json_stats: ' + json.dumps(log_table) + '\n')
-            #save latest network
+            # save latest network
             torch.save(network.state_dict(), '%s/network_last.pth' % (dir_name))
