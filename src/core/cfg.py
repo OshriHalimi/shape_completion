7b4045ebe6b343ab672692a7449091e451def3ac
OshriHalimi <champ8085@gmail>
2020-01-31 22:46:46 +0200

    more code review

--- src/core/cfg.py
+++ src/core/cfg.py
@@ -21,7 +21,9 @@ You can also concatenate - gtrb = Ground Truth Reconstruction Batched
 # ----------------------------------------------------------------------------------------------------------------------
 #                                              COMPUTATION CONFIG
 # ----------------------------------------------------------------------------------------------------------------------
-
+# TODO: I think it's better to save the config file for hard coded constants, paths and similar.
+#  Most of the variables defined here should be accessible from the experiment interface
+#  (it is confusing to split the experiment paramters into different files). If the experiment interface gets long we can order in neatly in a new file (not config)
 # TIP: Use CTRL + SHIFT + f in PyCharm to detect where these are used in the system
 
 RANDOM_SEED = 2147483647  # The global random seed. Use datetime.now() For a truly random seed
@@ -31,8 +33,8 @@ DEF_COMPUTE_PRECISION = 'float32' # Default computation precision used through o
 NORMAL_MAGNITUDE_THRESH = 10 ** (-6) # The minimal norm allowed for vertex normals to decide that they are too small
 
 # Hyper parameters that were removed from main.py
-DEF_LR_SCHED_COOLDOWN = 5 # Number of epoches to wait after reducing the step-size. Works only if LR sched is enabled
-DEF_MINIMAL_LR = 1e-6 # The smallest learning step allowed with LR sched. Works only if LR sched is enabled
+DEF_LR_SCHED_COOLDOWN = 5 # Number of epoches to wait after reducing the step-size. Works only if LR sched is enabled # TODO: remove to experiment interface. This is a learning rate paramter!
+DEF_MINIMAL_LR = 1e-6 # The smallest learning step allowed with LR sched. Works only if LR sched is enabled # TODO: remove to experiment interface. This is a learning rate paramter!
 NON_BLOCKING = True # Transfer to GPU in a non-blocking method
 REPORT_LOSS_PER_BATCH = False # If True - will output train loss to logger on every batch. Otherwise - on every epoch
 MAX_EPOCHS = 1000
@@ -53,6 +55,7 @@ DANGEROUS_MASK_THRESH = 100 # The minimal length allowed for mask vertex indices
 # ----------------------------------------------------------------------------------------------------------------------
 #                                               VISUALIZATION CONFIG
 # ----------------------------------------------------------------------------------------------------------------------
+# TODO: remove all to experiment interface
 VIS_METHOD = 'spheres' # spheres,cloud,mesh  - Choose how to display the meshes
 VIS_CMAP = 'summer' # https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
 # We use two colors: one for the mask verts [Right end of the spectrum] and one for the rest [Left end of the spectrum].
