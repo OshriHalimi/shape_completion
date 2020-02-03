added volume calculation. TODO: Fix tester functions in ops.py. And validate volume calculation and barycenter calculation.

src/core/architecture/loss.py
@@ -23,10 +23,7 @@ class F2PSMPLLoss:
             self.torch_f = torch.from_numpy(f).long().to(device=self.dev, non_blocking=self.non_blocking)
 
         # Sanity Check - Input Channels:
-        #TODO: we should have a transformation block operating on the initial data, adding input channels
-        #TODO: We should have a transformation block oprating on the network data, adding output channels
-        #TODO: the input and output channels should not be calculated in the loss stage - Loss.compute()
-
+        #TODO: we should have a transformation block operating on the initial data, adding input channels.
         #TODO: hp.in_channels should be defined with respect to transformed input data.
         # For example: the initial data might not have normals (initial input channels = 3), however in the input pipeline
         # we add normals (before the network), making the input channel = 6. Now, assume we want to calculate normal loss.
@@ -61,7 +58,7 @@ class F2PSMPLLoss:
         :param gtrb: The batched ground truth reconstruction of dim: [b x nv x 3]
         :return: The loss
         """
-        # TODO: Insert support for other out_channels: This codes assumes gtr has 3 input channels
+
         # Aliasing. We can only assume that channels 0:3 definitely exist
         gtrb_xyz = gtrb [:, :, 0:3]
         gtb_xyz = b['gt'][:, :, 0:3]
@@ -82,9 +79,6 @@ class F2PSMPLLoss:
                     vnb, is_valid_vnb = out[0:2]
                     if need_f_area:
                         f_area_gtrb = out[2]
-                    # TODO: The above chunk shouldn't be here (should be done by an output transformation block)
-                    # TODO: Currently we mix loss logic with transformation logic
-
                     loss += self._l2_loss(b['gt'][:, :, 3:6], vnb, lamb=lamb, vertex_mask=w*is_valid_vnb.unsqueeze(2))
                 elif i == 2:  # Moments:
                     loss += self._l2_loss(b['gt'][:, :, 6:12], batch_moments(gtrb), lamb=lamb, vertex_mask=w)
@@ -96,9 +90,6 @@ class F2PSMPLLoss:
                         f_area_gtrb
                     except NameError:
                         f_area_gtrb = batch_fnrmls_fareas(gtrb_xyz, self.torch_f, return_normals=False)
-                    # TODO: The above chunk shouldn't be here (should be done by an input/output transformation block)
-                    # TODO: Currently we mix loss logic with transformation logic
-
                     loss += self._l2_loss(f_area_gt, f_area_gtrb, lamb=lamb, vertex_mask=w)
                 elif i == 5:  # Volume:
                     pass

src/core/mesh/ops.py
@@ -2,13 +2,21 @@ import torch
 import numpy as np
 import torch.nn.functional as F
 from util.data_ops import normr, index_sparse
-from util.func import time_me
 import cfg
-from util.torch_nn import PytorchNet
 
 # ----------------------------------------------------------------------------------------------------------------------#
 #                                    Singleton Computes for Numpy/Pytorch Tensors
 # ----------------------------------------------------------------------------------------------------------------------#
+#TODO: validate me
+def calc_volume(v,f):
+    v1 = v[:,f[:,0],:]
+    v2 = v[:,f[:,1],:]
+    v3 = v[:,f[:,2],:]
+    a_vec = torch.cross(v2-v1, v3-v1, -1)
+    center = (v1+v2+v3)/3
+    volume = torch.sum(a_vec*center/6, dim=(1,2))
+    return volume
+
 def vf_adjacency(faces, n_faces, n_verts, device):
     """
     :param faces: dim: [N_faces x 3]
@@ -26,7 +34,7 @@ def vf_adjacency(faces, n_faces, n_verts, device):
     adjacency_vf = torch.sparse.IntTensor(ind.t(), ones_vec, torch.Size([n_verts, n_faces]))
     return adjacency_vf
 
-
+#TODO: validate me
 def face_barycenters(v, f):
     """
     :param v: vertices (numpy array or tensors), dim: [n_v x 3]
@@ -37,13 +45,15 @@ def face_barycenters(v, f):
     v2 = v[f[:, 1], :]  # dim: [n_faces x 3]
     v3 = v[f[:, 2], :]  # dim: [n_faces x 3]
 
-    center_x = v1[:, 0] + v2[:, 0] + v3[:, 0]
-    center_y = v1[:, 1] + v2[:, 1] + v3[:, 1]
-    center_z = v1[:, 2] + v2[:, 2] + v3[:, 2]
+    # center_x = v1[:, 0] + v2[:, 0] + v3[:, 0]
+    # center_y = v1[:, 1] + v2[:, 1] + v3[:, 1]
+    # center_z = v1[:, 2] + v2[:, 2] + v3[:, 2]
+    #
+    # centers = torch.stack((center_x, center_y, center_z), dim=1) if torch.is_tensor(center_x) \
+    #     else np.stack((center_x, center_y, center_z), axis=1)
+    # centers = (1 / 3) * centers  # Faster to do it here than for each center
 
-    centers = torch.stack((center_x, center_y, center_z), dim=1) if torch.is_tensor(center_x) \
-        else np.stack((center_x, center_y, center_z), axis=1)
-    centers = (1 / 3) * centers  # Faster to do it here than for each center
+    centers = (v1 + v2 + v3)/3
     return centers
 
 
@@ -248,7 +258,7 @@ def test_fnrmls_visually():
 
 
 if __name__ == '__main__':
-    test_vnrmls_visually()
+    test_fnrmls_visually()
 
 
 