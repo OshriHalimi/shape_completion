b989b4a757478bf751f9bd3fe2bcd89f43b68f7e
Ido Imanuel <ido.imanuel@gmail.com>
2020-02-10 17:00:38 +0200

    With full server instructions

--- src/server/interactive_run.bash
+++ src/server/interactive_run.bash
@@ -1,5 +1,6 @@
 #!/bin/bash 
-python3 ~/shape_completion/src/core/main.py
+source ~/shape_completion/src/server/server_setup.bash
+python ~/shape_completion/src/core/main.py 
 
 # Run on Server:Gaon3 + Queue:GIP for Time:3600 with one single gpu 
-# srun -w gaon3 -p gip --time=3600 --gres=gpu:1 --job-name="ShapeCompletion" --pty ~/shape_completion/src/core/interactive_run.bash
+# srun -w gaon3 -p gip --time=3600 --gres=gpu:1 --job-name="ShapeCompletion" --pty ~/shape_completion/src/server/interactive_run.bash
--- src/server/server_setup.bash
+++ src/server/server_setup.bash
@@ -1,11 +1,14 @@
 #!/bin/bash 
 #-------------------------------------------- On your first time: Manually --------------------------------------------#
+# Add "source ~/shape_completion/src/server/server_setup.bash" to the end of ~/.bashrc
 # git clone https://github.com/OshriHalimi/shape_completion.git shape_completion 
 # chmod 777 ~/shape_completion/src/server/*
-# Add source ~/shape_completion/src/server/server_setup.bash to ~/.bashrc
-# pip3 install --user --upgrade pip
-# pip3 install --user torch torchvision
-# pip3 install --user -r ~/shape_completion/requirements.txt
+# Install conda via: ~/shape_completion/src/server/Miniconda3-latest-Linux-x86_64.sh 
+# Make sure to type 'YES' to all 
+# Create a new conda env with: conda create -n shape_completion python==3.7.6
+# Activate the environment: conda activate shape_completion
+# conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
+# pip install -r ~/shape_completion/requirements.txt
 #----------------------------------------------------------------------------------------------------------------------#
 # Basic Tutorial: 
 # 8 Gaon Servers, ~4 GPUs per Server
@@ -13,7 +16,12 @@
 # Max Jobs - Between 3 & 5 
 # Server IP:
 SERVER_IP=`hostname -I | sed 's/ *$//g'`
-echo "Use $SERVER_IP:6006 to log-on tensorboard"
+echo "======================================= Server Instructions ========================================"
+echo "Surf to $SERVER_IP:6006 using any browser to see tensorboard output"
+echo "Use the squeue command to see queue status"
+echo "Use the sinfo command to see the server status. down+drain = bad, mix + idle = good"
+echo "===================================================================================================="
+
 
 # Aliasing: 
 alias squeue='squeue -o "%.18i %.9P %.8j %.8u %.2t %.10M %.6D %R %Q"'
