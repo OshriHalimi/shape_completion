54ae0d592373d1173491062108c85f359421a2ee
smosesli <smosesli@campus.technion.ac.il>
2020-02-17 20:13:21 +0200

    mixamo generation is running, still there is a bug in rendering for many files

--- src/data_prep/external_tools/pyRender/src/gen_projections.py
+++ src/data_prep/external_tools/pyRender/src/gen_projections.py
@@ -1,129 +1,84 @@
-"""
-Generate projections
-usage:
-cd into pyRender/src
-then run:
-python gen_projections.py ./../resources/human.off --output_path='./projections/' --output='mask'
-if you want to save the pointclouds for visualization, use --output='ply'
-"""
-
-# pyrender can be found at: https://github.com/hjwdzh/pyRender
-
 import numpy as np
 import sys
-import skimage.io as sio
 import os
-import sys
-import shutil
-from objloader import LoadTextureOBJ
-import time
-import gc
+import os.path
+import trimesh
+import pathlib
+from core.util.mesh.plot import plot_mesh
+import matplotlib.pyplot as plt
+from data_prep.external_tools.pyRender.lib import render
 
-libpath = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(libpath + '/../lib')
-import render
-import objloader
-# import pc_utils
-from pc_utils import write_ply
-import pdb
-import argparse
+def run(filename, out_dir,error_path, scale):
 
+	pc = trimesh.load(filename,'obj')
+	V = np.array(pc.vertices) * scale
+	F = np.array(pc.faces)
+	bbox_x = [np.min(V[:, 0]), np.max(V[:, 0])]
+	bbox_y = [np.min(V[:, 1]), np.max(V[:, 1])]
+	bbox_z = [np.min(V[:, 2]), np.max(V[:, 2])]
+	center = 0.5 * np.array([bbox_x[0] + bbox_x[1], bbox_y[0] + bbox_y[1], bbox_z[0] + bbox_z[1]])
+	V = V - np.expand_dims(center, axis=0)
+	V = V.astype(np.float32)
+	F = F.astype(np.int32)
 
-
-
-# input_obj = sys.argv[1]
-
-#parser = argparse.ArgumentParser()
-#parser.add_argument('--output_path', default='./data/', help='[ply, mask]')
-#parser.add_argument('--output', default='ply', help='[ply, mask]')
-#parser.add_argument('--num_ang',type=int, default=10, help='')
-#parser.add_argument('--filename',default='./../resources/human.off', help='')
-
-#args = parser.parse_args()
-
-
-#f = open("file_list.txt", "r")
-#files = [line.strip("\n") for line in f]
-#f.close()
-
-
-files = os.listdir(os.path.join(os.getcwd(), os.pardir, "resources", "input_data", "train"))
-
-for file in files:
-
-	t = time.time()
-
-	off_file = os.path.join(os.getcwd(), os.pardir, "resources", "input_data", "train", file)
-	V, F = objloader.LoadOff(off_file)
-	print(np.min(V), np.max(V))
-
-
-#if args.filename[-3:] == 'obj':
-#	V, F, VT, FT, VN, FN, face_mat, kdmap = objloader.LoadTextureOBJ(input_obj)
-#else:
-#	V, F = objloader.LoadOff(args.filename)
-#
-#print(np.min(V), np.max(V))
-
-
-
-	# set up camera information',
+	# set up camera information,
 	info = {'Height':480, 'Width':640, 'fx':575, 'fy':575, 'cx':319.5, 'cy':239.5}
 	render.setup(info)
 
 	# set up mesh buffers in cuda
 	context = render.SetMesh(V, F)
-
 	cam2world = np.array([[ 0.85408425,  0.31617427, -0.375678  ,  0.56351697 * 2],
 		   [ 0.        , -0.72227067, -0.60786998,  0.91180497 * 2],
 		   [-0.52013469,  0.51917219, -0.61688   ,  0.92532003 * 2],
 		   [ 0.        ,  0.        ,  0.        ,  1.        ]], dtype=np.float32)
 
 
+	# TODO: add options for more elevations
 	# rotate the mesh elevation by 30 degrees
 	Rx = np.array([[ 1, 0, 0, 0],
-	           [ 0.        , np.cos(np.pi/6), -np.sin(np.pi/6), 0],
-	           [0, np.sin(np.pi/6),  np.cos(np.pi/6), 0],
-	           [ 0.        ,  0.        ,  0.        ,  1.        ]], dtype=np.float32)
-
+			   [ 0.        , np.cos(np.pi/6), -np.sin(np.pi/6), 0],
+			   [0, np.sin(np.pi/6),  np.cos(np.pi/6), 0],
+			   [ 0.        ,  0.        ,  0.        ,  1.        ]], dtype=np.float32)
 	cam2world = np.matmul(Rx,cam2world)
 
-
+	# TODO: add option for different angles
 	# rotate along y axis and render
 	for i_ang, ang in enumerate(np.linspace(0,2*np.pi, 10)):
 
-	    Ry = np.array([[ np.cos(ang),  0, -np.sin(ang)  ,  0],
-	           [ 0.        , 1, 0,  0],
-	           [np.sin(ang),  0, np.cos(ang)   ,  0],
-	           [ 0.        ,  0.        ,  0.        ,  1.        ]], dtype=np.float32)
-
-	    world2cam = np.linalg.inv(np.matmul(Ry,cam2world)).astype('float32')
-
-
-	    # the actual rendering process
-	    render.render(context, world2cam)
-
-	    # get depth information
-	    depth = render.getDepth(info)
-
-	    # get information of mesh rendering
-	    # vindices represents 3 vertices related to pixels
-	    # vweights represents barycentric weights of the 3 vertices
-	    # findices represents the triangle index related to pixels
-	    vindices, vweights, findices = render.getVMap(context, info)
-	    mask = np.unique(vindices)
-	    if len(mask) == 1:
-	    	print(file)
+		Ry = np.array([[ np.cos(ang),  0, -np.sin(ang)  ,  0],
+			   [ 0.        , 1, 0,  0],
+			   [np.sin(ang),  0, np.cos(ang)   ,  0],
+			   [ 0.        ,  0.        ,  0.        ,  1.        ]], dtype=np.float32)
+		world2cam = np.linalg.inv(np.matmul(Ry,cam2world)).astype('float32')
 
 
-	    output_path = os.path.join(os.getcwd(), "data_output_train")
-	    if not os.path.exists(output_path):
-	        os.makedirs(output_path)
+		# the actual rendering process
+		render.render(context, world2cam)
 
-	    out_name = file[:-4] + "_" + str(i_ang)
-	    np.savez(os.path.join(output_path, out_name), mask=mask)
-	    
-	render.Clear()
-	gc.collect()
+		# get depth information
+		depth = render.getDepth(info)
+		#plt.imshow(depth)
 
-	print(f"time: {time.time() - t}")
+		# get information of mesh rendering
+		# vindices represents 3 vertices related to pixels
+		# vweights represents barycentric weights of the 3 vertices
+		# findices represents the triangle index related to pixels
+		vindices, vweights, findices = render.getVMap(context, info)
+		mask = np.unique(vindices)
+		if len(mask) == 1:
+			print("ERROR: WRONG MASK !!!", filename)
+			if not os.path.isfile(error_path):
+				open(error_path, "x")
+			f = open(error_path, "a")
+			f.write(filename + "\n")
+			f.close()
+			break
+
+
+		out_name = os.path.join(out_dir, f"{str(i_ang)}")
+		pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
+		#plot_mesh(V[mask,:], strategy='spheres', grid_on=True)
+
+		np.savez(out_name, mask=mask)
+		print(out_name)
+		render.Clear()
